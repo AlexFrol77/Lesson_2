7f67b85d2d223b6fd2395b30f486d9756e0627c9
Alex Frol <drbo19777@gmail.com>
2022-11-15 14:36:49 +0300

    Init commit

---
+++ Task_1/Task_1/Task_1.cpp
@@ -0,0 +1,77 @@
+﻿#include <iostream>
+#include <string>
+#include <windows.h>
+
+enum class Mon {
+	Jan = 1,
+	Feb,
+	March,
+	April,
+	May,
+	June,
+	July,
+	Aug,
+	Sept,
+	Oct,
+	Nov,
+	Dec
+};
+
+
+int main(int argc, char** argv) {
+	setlocale(LC_ALL, "Rus");
+	int temp;
+	do {
+		std::cout << "Введите номер месяца: ";
+		std::cin >> temp;
+		if (temp < 0 || temp > 12) {
+			std::cout << "Неправильный номер!" << std::endl;
+		}
+		else {
+			switch (temp) {
+			case static_cast<int>(Mon::Jan):
+				std::cout << "Январь" << std::endl;
+				break;
+			case static_cast<int>(Mon::Feb):
+				std::cout << "Февраль" << std::endl;
+				break;
+			case static_cast<int>(Mon::March):
+				std::cout << "Март" << std::endl;
+				break;
+			case static_cast<int>(Mon::April):
+				std::cout << "Апрель" << std::endl;
+				break;
+			case static_cast<int>(Mon::May):
+				std::cout << "Май" << std::endl;
+				break;
+			case static_cast<int>(Mon::June):
+				std::cout << "Июнь" << std::endl;
+				break;
+			case static_cast<int>(Mon::July):
+				std::cout << "Июль" << std::endl;
+				break;
+			case static_cast<int>(Mon::Aug):
+				std::cout << "Август" << std::endl;
+				break;
+			case static_cast<int>(Mon::Sept):
+				std::cout << "Сентябрь" << std::endl;
+				break;
+			case static_cast<int>(Mon::Oct):
+				std::cout << "Октябрь" << std::endl;
+				break;
+			case static_cast<int>(Mon::Nov):
+				std::cout << "Ноябрь" << std::endl;
+				break;
+			case static_cast<int>(Mon::Dec):
+				std::cout << "Декабрь" << std::endl;
+				break;
+
+			default:
+				break;
+			}
+		}
+	} while (temp != 0);
+	std::cout << "До свидания" << std::endl;
+	Sleep(2000);
+	return 0;
+}
---
+++ Task_2/Task_2/Task_2.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <string>
+#include <windows.h>
+
+struct Credit {
+	int UserScore;
+	std::string UserName;
+	double UserSizeScore;
+};
+
+void setData(Credit* obj) {
+	std::cout << "Введите номер счёта: ";
+	std::cin >> obj->UserScore;
+	std::cout << "Введите имя владельца: ";
+	std::cin >> obj->UserName;
+	std::cout << "Введите баланс: ";
+	std::cin >> obj->UserSizeScore;
+}
+void setNewSizeScore(Credit& obj) {
+	std::cout << "Введите новый баланс: ";
+	std::cin >> obj.UserSizeScore;
+}
+void print(Credit& obj) {
+	std::cout << "Ваш счёт: " << obj.UserName << " " << obj.UserScore << " " << obj.UserSizeScore << std::endl;
+}
+
+int main(int argv, char** argc) {
+
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	Credit User;
+	setData(&User);
+	setNewSizeScore(User);
+	print(User);
+	return 0;
+}
